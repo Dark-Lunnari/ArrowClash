acddecb7adbdc76db35ccc844f71caeee2d61d72
Aydan Guzevicius <aydan.guzevicius20@alumni.loyola.ca>
2021-12-07 00:47:13 -0500

    Added Title and Grass
    Added title and Grass

--- Arrow Clash/MyWorld.java
+++ Arrow Clash/MyWorld.java
@@ -20,6 +20,22 @@ public class MyWorld extends World
         addObject(player,90, getHeight() - 100);
         //Boss boss = new Boss();
         //addObject(boss,800,190);
-        DeflectedArrow DeflectedArrow = new DeflectedArrow(); 
+        DeflectedArrow DeflectedArrow = new DeflectedArrow();         
+        Grass Grass5 =  new  Grass();
+        addObject(Grass5, 114, 359);
+        Grass Grass6 =  new  Grass();
+        addObject(Grass6, 295, 353);
+        Grass Grass7 =  new  Grass();
+        addObject(Grass7, 480, 358);
+        Grass Grass8 =  new  Grass();
+        addObject(Grass8, 596, 359);
+        Grass Grass9 =  new  Grass();
+        addObject(Grass9, 740, 354);
     }
-}
+    
+    public void act(){       
+        if (Greenfoot.getRandomNumber(100) < 1) {
+            addObject( new  Grass(), 779, 359);
+        }
+      }
+    }
